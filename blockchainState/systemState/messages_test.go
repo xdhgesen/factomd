75c9b665a94f301d00be2a2de3dc5d9820e4f9a7
ThePiachu <ThePiachu@gmail.com>
2017-09-05 07:52:24 -0700

    Adding new test

--- blockchainState/systemState/messages_test.go
+++ blockchainState/systemState/messages_test.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/FactomProject/factomd/blockchainState"
 	. "github.com/FactomProject/factomd/blockchainState/systemState"
+	"github.com/FactomProject/factomd/common/primitives"
 	"github.com/FactomProject/factomd/testHelper"
 )
 
@@ -42,5 +43,40 @@ func TestProcessDBStateMessage(t *testing.T) {
 	//(can't process the last one since we don't have the proper signatures!)
 	if ss.BStateHandler.MainBState.DBlockHeight != uint32(prev.Height-1) {
 		t.Errorf("Wrong DBlockHeight - %v vs %v", ss.BStateHandler.MainBState.DBlockHeight, uint32(prev.Height))
+	}
+}
+
+func TestProcessBlockMessageSet(t *testing.T) {
+	ss := new(SystemState)
+	ss.BStateHandler = new(BStateHandler)
+	ss.BStateHandler.MainBState = blockchainState.NewBSLocalNet()
+	ss.BStateHandler.DB = testHelper.CreateEmptyTestDatabaseOverlay()
+
+	blocks := testHelper.CreateFullTestBlockSet()
+	sl := testHelper.GetSigListFromBlockSet(blocks[1])
+	msg := testHelper.BlockSetToDBStateMsg(blocks[0], sl)
+
+	err := ss.ProcessMessage(msg)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	priv, err := primitives.NewPrivateKeyFromHex("4c38c72fc5cdad68f13b74674d3ffb1f3d63a112710868c9b08946553448d26d")
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	messages, acks := testHelper.BlockSetToMessageList(blocks[1], priv)
+	for _, v := range messages {
+		err = ss.ProcessMessage(v)
+		if err != nil {
+			t.Errorf("%v", err)
+		}
+	}
+	for _, v := range acks {
+		err = ss.ProcessMessage(v)
+		if err != nil {
+			t.Errorf("%v", err)
+		}
 	}
 }
