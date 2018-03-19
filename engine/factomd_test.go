fdd24e3d1c57b545a81b33d4423e9c90a18556a1
Clay Douglass <clay@factom.com>
2018-03-19 17:04:14 -0500

    Added logfiles to TestAnNetwork

--- engine/factomd_test.go
+++ engine/factomd_test.go
@@ -89,9 +89,11 @@ func TestSetupANetwork(t *testing.T) {
 		"-ControlPanelPort=37002",
 		"-networkPort=37003",
 		"-startdelay=1",
-		)
+	)
 
 	params := ParseCmdLine(args)
+
+	HandleLogfiles("out.txt", "out.txt")
 	state0 := Factomd(params, false).(*state.State)
 	state0.MessageTally = true
 	time.Sleep(3 * time.Second)
@@ -255,7 +257,7 @@ func TestAnElection(t *testing.T) {
 		"-blktime=10",
 		fmt.Sprintf("-count=%d", nodes),
 		"-startdelay=1",
-                "-debuglog=F.*",
+		"-debuglog=F.*",
 	)
 	HandleLogfiles("out.txt", "out.txt")
 	params := ParseCmdLine(args)
@@ -288,7 +290,7 @@ func TestAnElection(t *testing.T) {
 			break
 		}
 		fmt.Printf("Waiting for G5 to complete\n")
-	WaitMinutes(state0, 1)
+		WaitMinutes(state0, 1)
 
 	}
 	// Allocate leaders
