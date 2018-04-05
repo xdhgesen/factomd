28d55a85422740ebb5b0277086e43af8e94d7dba
Clay Douglass <clay@factom.com>
2018-04-05 11:24:36 -0500

    Fixed capitolization of parameters that were missed

--- engine/factomd_test.go
+++ engine/factomd_test.go
@@ -96,9 +96,12 @@ func TestSetupANetwork(t *testing.T) {
 		"-count=10",
 		"-logPort=37000",
 		"-port=37001",
-		"-ControlPanelPort=37002",
-		"-networkPort=37003",
+		"-controlpanelport=37002",
+		"-networkport=37003",
 		"-startdelay=1",
+		//"-debuglog=F.*",
+		"--stdoutlog=out.txt",
+		"--stderrlog=out.txt",
 	)
 
 	params := ParseCmdLine(args)
