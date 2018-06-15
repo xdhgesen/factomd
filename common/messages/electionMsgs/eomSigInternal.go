c142abf00a2341826e919dab9a9c565ede530ad3
Clay Douglass <clay@factom.com>
2018-06-15 15:19:45 -0500

    Solution is not to sort in election at EOM internal and not to sort in process except at the start of a block

--- common/messages/electionMsgs/eomSigInternal.go
+++ common/messages/electionMsgs/eomSigInternal.go
@@ -9,7 +9,6 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/FactomProject/factomd/CheckAuth"
 	"github.com/FactomProject/factomd/common/constants"
 	"github.com/FactomProject/factomd/common/interfaces"
 	"github.com/FactomProject/factomd/common/messages/msgbase"
@@ -138,12 +137,7 @@ func (m *EomSigInternal) ElectionProcess(is interfaces.IState, elect interfaces.
 		// Set the title in the state
 		s.Election0 = Title()
 
-		//		if m.ComparisonMinute() != 0 {
-		// Sort leaders, an election is previous min/block may mess up ordering
-		elections.Sort(e.Federated)
-		elections.Sort(e.Audit)
-		checkAuth.CheckAuthSetsMatch("ElectionProcess(SORT)", e.Federated, e.Audit, s.GetFedServers(uint32(e.DBHeight)), s.GetAuditServers(uint32(e.DBHeight)), s)
-		//		}
+		//		checkAuth.CheckAuthSetsMatch("ElectionProcess(SORT)", e.Federated, e.Audit, s.GetFedServers(uint32(e.DBHeight)), s.GetAuditServers(uint32(e.DBHeight)), s)
 
 		e.FaultId.Store(e.FaultId.Load() + 1) // increment the timeout counter
 		go Fault(e, e.DBHeight, e.Minute, e.FaultId.Load(), &e.FaultId, m.SigType, e.Timeout)
