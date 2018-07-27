e7c5193025fb5cf6cace330a4e5b6dcf151c0098
Paul Snow <snow.paul@gmail.com>
2018-07-27 14:37:13 -0500

    fix for better management of the entrydbheightcomplete state for faster loading

--- state/state.go
+++ state/state.go
@@ -526,6 +526,15 @@ func (s *State) Clone(cloneNumber int) interfaces.IState {
 		newState.StateSaverStruct.FastBootLocation = newState.BoltDBPath
 		break
 	}
+
+	height, err := s.DB.FetchDatabaseEntryHeight()
+	if err != nil {
+		os.Stderr.WriteString(fmt.Sprintf("%10s ERROR: %v\n", s.FactomNodeName, err))
+	} else {
+		os.Stderr.WriteString(fmt.Sprintf("%10s loaded to %8d\n", s.FactomNodeName, height))
+		s.EntryDBHeightComplete = height
+	}
+
 	return newState
 }
 
