028b635a83df0bd54707fecd9d818edba5049b5a
Matt York <matt.york@factom.com>
2018-09-07 08:25:48 -0500

    dump mem pprof to file once a second

--- factomd.go
+++ factomd.go
@@ -5,14 +5,43 @@
 package main
 
 import (
-	"os"
-
 	"fmt"
+	"os"
 	"runtime"
+	"runtime/pprof"
 	"time"
 
 	. "github.com/FactomProject/factomd/engine"
 )
+
+func MemProfiler() {
+	// record memory profile on a loop
+	ticker := time.NewTicker(time.Second)
+
+	for {
+		<-ticker.C
+		st := &runtime.MemStats{}
+		runtime.ReadMemStats(st)
+		// From Golang docs: HeapObjects increases as objects are allocated
+		// and decreases as the heap is swept and unreachable objects are
+		// freed.
+		/*
+			fmt.Println("Heap allocs:", st.Mallocs, "Heap frees:",
+				st.Frees, "Heap objects:", st.HeapObjects)
+		*/
+
+		// REVIEW: couldn't figure out how to collect multiple profiles in the same file
+		if f, err := os.Create(fmt.Sprintf("factomd.%v.memprof", time.Now().UnixNano())); err != nil {
+			fmt.Printf("record memory profile failed: %v", err)
+		} else {
+			runtime.GC() // trigger garbage collection first
+			pprof.WriteHeapProfile(f)
+			f.Close()
+			//fmt.Println("record memory profile")
+		}
+
+	}
+}
 
 func main() {
 	// uncomment StartProfiler() to run the pprof tool (for testing)
@@ -20,6 +49,8 @@ func main() {
 
 	//  Go Optimizations...
 	runtime.GOMAXPROCS(runtime.NumCPU()) // TODO: should be *2 to use hyperthreadding? -- clay
+
+	go MemProfiler()
 
 	fmt.Printf("Arguments\n %+v\n", params)
 
