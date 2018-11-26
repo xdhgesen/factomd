555a6e144bbc28487b4c76576c91fbfce7ace6b3
stackdump <myork@stackdump.com>
2018-11-26 14:01:53 -0600

    added failing test

--- simTest/FactoidPermBalanceWithElection_test.go
+++ simTest/FactoidPermBalanceWithElection_test.go
@@ -14,7 +14,7 @@ var depositAddresses []string
 
 // generate addresses & private keys
 func createDepositAddresses() {
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 1; i++ {
 		_, addr := RandomFctAddressPair()
 		depositAddresses = append(depositAddresses, addr)
 	}
@@ -26,33 +26,19 @@ func TestPermFCTBalancesAfterMin9Election(t *testing.T) {
 	}
 	RanSimTest = true
 	createDepositAddresses()
-	state0 := SetupSim("LLAL", map[string]string{"--debuglog": "", "--faulttimeout": "10"}, 8, 1, 1, t)
+	state0 := SetupSim("LLAL", map[string]string{"--debuglog": "", "--faulttimeout": "10"}, 10, 1, 1, t)
 
-	quitDepositor := make(chan int)
-	var depositCount uint64 = 0
+	var depositCount int64 = 0
+	var ecPrice uint64 = state0.GetFactoshisPerEC() //10000
 
-	// generate deposits from bank account
-	go func() {
-		var ecPrice uint64 = state0.GetFactoshisPerEC() //10000
-
-		for {
-			select {
-			case <-quitDepositor:
-				println("stop depositor")
-				return
-			default:
-				depositCount += 1
-				// FIXME add a way to exit this loop
-				for i := range depositAddresses {
-					fmt.Printf("TXN %v %v => %v \n", depositCount, depositAddresses[i], depositAddresses[i])
-					time.Sleep(time.Millisecond*90)
-					SendTxn(state0, 1, bankSecret, depositAddresses[i], ecPrice)
-				}
-				WaitMinutes(state0, 1)
-			}
+	mkTransactions := func() {
+		depositCount++
+		for i := range depositAddresses {
+			fmt.Printf("TXN %v %v => %v \n", depositCount, depositAddresses[i], depositAddresses[i])
+			time.Sleep(time.Millisecond*90)
+			SendTxn(state0, 1, bankSecret, depositAddresses[i], ecPrice)
 		}
-
-	}()
+	}
 
 	t.Run("trigger election at min 9", func(t *testing.T) {
 		StatusEveryMinute(state0)
@@ -65,23 +51,27 @@ func TestPermFCTBalancesAfterMin9Election(t *testing.T) {
 		RunCmd("3")
 		WaitForMinute(state3, 9) // wait till the victim is at minute 9
 		RunCmd("x")
+		go mkTransactions()
 		WaitMinutes(state0, 1) // Wait till fault completes
 		RunCmd("x")
-
 		WaitBlocks(state0, 2)    // wait till the victim is back as the audit server
 		WaitForMinute(state0, 1) // Wait till ablock is loaded
 		WaitForAllNodes(state0)
-		quitDepositor <- 0 // stop making transactions
 		WaitForMinute(state3, 1) // Wait till node 3 is following by minutes
+		//WaitBlocks(state3, 2)
 
-		// REVIEW: make sure we are actually testing the correct scenario
+		WaitForAllNodes(state0)
+		ShutDownEverything(t)
+
 		t.Run("check permanent balances for addresses on each node", func(t *testing.T) {
-			// FIXME
-			assert.Nil(t, nil)
-			// TOSO: check permanent balances for all addresses on each node
-			// depositCount should be the same amount in each account
+			for i, node := range GetFnodes() {
+				for _, addr := range depositAddresses {
+					bal := GetBalance(node.State, addr)
+					msg := fmt.Sprintf("Node%v %v => balance: %v expected: %v \n", i, addr, bal, depositCount)
+					assert.Equal(t, depositCount, bal, msg)
+				}
+			}
+
 		})
 	})
-	WaitForAllNodes(state0)
-	ShutDownEverything(t)
 }
