0687a52a5e3a8dbf7673f382ea5a938ba18a4665
Paul Snow <snow.paul@gmail.com>
2019-01-09 14:43:12 -0600

    Added usage comments

--- scripts/SaveFastBoots.sh
+++ scripts/SaveFastBoots.sh
@@ -1,3 +1,9 @@
+#
+# Remove any fastboot files from your M2 directory.  
+# Run this script in your M2 directory
+# Run factomd against the main net
+# All the fast boot files will be preserved in m2/fastboots
+#
 rm -r ./fastboots
 mkdir ./fastboots
 FILE=FastBoot_MAIN_v10.db
