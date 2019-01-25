30c9b8743fab72ed5f379211a7bcc69ac0ea0355
stackdump <myork@stackdump.com>
2019-01-25 16:10:42 -0600

    WIP

--- simTest/dataload.py
+++ simTest/dataload.py
@@ -6,64 +6,47 @@ from pprint import pprint
 NODES = ['fnode0']
 
 LOGS = [
-    'ackqueue',
-    'apilog',
-    #'balancehash',
-    'commits',
-    #'dbsig-eom',
-    #'dbsig',
-    #'dbstateprocess',
-    #'duplicatesend',
-    #'election',
-    #'entrycredits',
-    'entrycredits_trans',
-    #'entrysync',
-    #'executemsg',
-    #'factoids',
-    #'factoids_trans',
-    'faulting',
-    'holding',
-    'inmsgqueue',
-    'inmsgqueue2',
-    'missing_messages',
-    'msgqueue',
-    'networkinputs',
-    'networkoutputs',
-    #'pendingchainheads',
-    #'process',
-    #'processlist',
-    #'processstatus',
     'simtest',
+    #'processstatus',
+    #'processlist',
+    #'process',
+    #'pendingchainheads',
+    #'networkoutputs', # 500k+ load by itself
+    #'networkinputs', # 100k+
+    'msgqueue',
+    'missing_messages',
+    'inmsgqueue2',
+    'inmsgqueue',
+    'holding',
+    'faulting',
+    #'factoids_trans',
+    #'factoids',
+    #'executemsg',
+    #'entrysync',
+    'entrycredits_trans',
+    #'entrycredits',
+    #'election',
+    #'duplicatesend',
+    #'dbstateprocess',
+    #'dbsig',
+    #'dbsig-eom',
+    'commits',
+    #'balancehash',
+    'apilog',
+    'ackqueue',
 ]
-
-#LOGS = [ 'missing']
 
 def main():
     """ initalize the db """
 
-    def x(sql, fetch=True):
-        conn = psycopg2.connect("postgres://load:load@localdb:5432")
-        cursor = conn.cursor()  
-        cursor.execute(sql)
-        rows = []
-
-        if fetch:
-            rows = cursor.fetchall()  
-
-        conn.commit()
-        conn.close()
-        return rows
-
-    def perform(sql):
-        return x(sql, fetch=False)
-
     def load(data, log, node):
         """ load data """
         try:
-            _ = json.loads(data)
-            perform("INSERT INTO %s.%s (e) values('%s')" % (node, log, data))
+            #_ = json.loads(data)
+            if data[0] in ['[', '{']:
+                x("INSERT INTO %s.%s (e) values('%s')" % (node, log, data))
         except Exception as ex:
-            #print(ex)
+            print(ex)
             print(log, node, data)
 
     def extract(log, node):
@@ -74,21 +57,30 @@ def main():
 
         fo.close()
 
-    perform('DROP TABLE IF EXISTS logs CASCADE')
-    perform('CREATE TABLE public.logs (e jsonb)')
+    conn = psycopg2.connect("postgres://load:load@localdb:5432")
+
+    def x(sql, fetch=True):
+        cursor = conn.cursor()  
+        cursor.execute(sql)
+        conn.commit()
+
+    x('DROP TABLE IF EXISTS logs CASCADE')
+    x('CREATE TABLE public.logs (e jsonb)')
 
     for n in NODES:
-        perform('DROP SCHEMA IF EXISTS %s CASCADE' % n)
-        perform('CREATE SCHEMA %s' % n)
+        x('DROP SCHEMA IF EXISTS %s CASCADE' % n)
+        x('CREATE SCHEMA %s' % n)
 
     for l in LOGS:
         for n in NODES:
             try:
-                perform('CREATE TABLE %s.%s () INHERITS (public.logs)' % (n, l))
+                x('CREATE TABLE %s.%s () INHERITS (public.logs)' % (n, l))
                 extract(l, n)
             except Excetion as ex:
                 print(ex)
                 print('skipped %s_%s' %(n,l))
+
+    conn.close()
 
 if __name__ == '__main__':
     main()
--- simTest/report.sql
+++ simTest/report.sql
@@ -1,5 +1,35 @@
 
-CREATE TABLE logs ( e jsonb);
+
+\x
+
+/*
+CREATE OR REPLACE FUNCTION ptnetState(varchar, varchar) RETURNS SETOF states
+AS $$
+DECLARE
+BEGIN
+  RETURN QUERY EXECUTE format('SELECT * FROM
+    %I.states 
+  WHERE
+    ptnet = %L AND oid = %L
+  ', $1, $1, $2);
+  
+END;
+$$ LANGUAGE plpgsql;
+*/
 
 
+--SELECT * FROM fnode0.simtest limit 1;
+
+CREATE OR REPLACE FUNCTION simtest_report() RETURNS SETOF RECORD as $$
+  SELECT
+    e->'ts' as ts,
+    e->'seq' as seq,
+    e->'log'->'height' as block, 
+    e->'log'->'min' as min, 
+    e->'log'->'event' as event, *
+  FROM
+    fnode0.simtest; 
+$$ LANGUAGE sql;
+
+SELECT simtest_report()
 
