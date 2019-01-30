0f02f53f4849614750d07c7549cd4bd8662183ac
stackdump <myork@stackdump.com>
2019-01-29 20:02:24 -0600

    WIP

--- test.sh
+++ test.sh
@@ -18,7 +18,7 @@ PACKAGES=('./simTest/...')
 FAIL=""
 
 for PKG in ${PACKAGES[*]} ; do
-  go test -v -vet=off -timeout 99m $PKG &> ./test.out
+  go test -v -vet=off -timeout 99m $PKG &>> ./test.out
   if [[ $? != 0 ]] ;  then
     FAIL=1
   fi
@@ -26,11 +26,11 @@ done
 
 if [[ "${FAIL}x" != "x" ]] ; then
   echo "TESTS FAIL"
-  cat ./test.out
+  #cat ./test.out
   exit 1
 else
   echo "-------------------DETAILED LOG-------------------"
-  cat simTest/fnode0_simtest.txt
+  #cat simTest/fnode0_simtest.txt
   echo "ALL TESTS PASS"
   exit 0
 fi
