7b9b65c2e3a8a73564c133f697b6a2655581c2d3
stackdump <myork@stackdump.com>
2019-04-23 11:40:33 -0500

    using this test to bench against FD-967

--- longTest/BootWithDB_test.go
+++ longTest/BootWithDB_test.go
@@ -7,16 +7,15 @@ import (
 	. "github.com/FactomProject/factomd/testHelper"
 )
 
-var givenNodes string = "LLLLLLLLAAAAFF"
-
+var givenNodes string = "LLLLLLLLFFFFFF"
 
 // Run this first to configure network and initialize databases
 func TestSetupBootWithoutDB(t *testing.T) {
 	// FIXME: later versions of factomd test suite will create a home directory based on test name
 	// this step should be relocated into TestBootWithDB function
-	params := map[string]string {
-		"--db":           "LDB",
-		"--net":          "alot+",
+	params := map[string]string{
+		"--db":  "LDB",
+		"--net": "alot+",
 	}
 	state0 := SetupSim(givenNodes, params, 10, 0, 0, t)
 	WaitBlocks(state0, 1)
@@ -29,11 +28,11 @@ Replicates behavior of
 factomd  --network=LOCAL --fastsaverate=100 --checkheads=false --count=15 --net=alot+ --blktime=600 --faulttimeout=12 --enablenet=false --startdelay=2 $@ > out.txt 2> err.txt
 */
 func TestBootWithDB(t *testing.T) {
-	params := map[string]string {
+	params := map[string]string{
 		"--db":           "LDB",
 		"--fastsaverate": "100",
 		"--net":          "alot+",
-		"--blktime":      "600",
+		"--blktime":      "30",
 		"--faulttimeout": "12",
 		"--startdelay":   "2",
 	}
@@ -51,9 +50,6 @@ func TestBootWithDB(t *testing.T) {
 
 	RunCmd("R5")
 
-	// FIXME should be able to run for a set number of blocks
-	time.Sleep(time.Second * 6000)
-	_ = state0
-	//WaitBlocks(state0, 5)
-	//ShutDownEverything(t)
+	time.Sleep(time.Second * 300)
+	t.Logf("LLHT: %v<=>%v", state0.GetDBHeightAtBoot(), state0.GetDBHeightComplete())
 }
