2c6c0394242b907c9858fde7dadff687f821bf50
Clay Douglass <clay@factom.com>
2019-05-09 12:34:01 -0500

    threaded message sort

--- state/validation.go
+++ state/validation.go
@@ -17,150 +17,130 @@ import (
 
 var ValidationDebug bool = false
 
+// This is the tread with access to state. It does process and update state
+func (s *State) DoProcessing() {
+	s.validatorLoopThreadID = atomic.Goid()
+	s.IsRunning = true
+
+	slp := false
+	i3 := 0
+
+	for s.IsRunning {
+
+		p1 := true
+		p2 := true
+		i1 := 0
+		i2 := 0
+
+		if ValidationDebug {
+			s.LogPrintf("executeMsg", "start validate.process")
+		}
+		for i1 = 0; p1 && i1 < 20; i1++ {
+			p1 = s.Process()
+		}
+		if ValidationDebug {
+			s.LogPrintf("executeMsg", "start validate.updatestate")
+		}
+		for i2 = 0; p2 && i2 < 20; i2++ {
+			p2 = s.UpdateState()
+		}
+
+		if !p1 || p2 {
+			// No work? Sleep for a bit
+			time.Sleep(10 * time.Millisecond)
+			s.ValidatorLoopSleepCnt++
+			i3++
+			slp = true
+		} else if slp {
+			slp = false
+			if ValidationDebug {
+				s.LogPrintf("DoProcessing", "slept %d times", i3)
+				i3 = 0
+			}
+
+		}
+
+	}
+	fmt.Println("Closing the Database on", s.GetFactomNodeName())
+	s.DB.Close()
+	s.StateSaverStruct.StopSaving()
+	fmt.Println(s.GetFactomNodeName(), "closed")
+}
+
 func (state *State) ValidatorLoop() {
 	CheckGrants()
 	timeStruct := new(Timer)
-	var prev time.Time
-	state.validatorLoopThreadID = atomic.Goid()
-	for {
-		s := state
-		if state.DebugExec() {
-			status := ""
-			now := time.Now()
-			if now.Sub(prev).Minutes() > 1 {
-				state.LogPrintf("executeMsg", "Timestamp DBh/VMh/h %d/%d/%d", state.LLeaderHeight, state.LeaderVMIndex, state.CurrentMinute)
-				pendingEBs := 0
-				pendingEntries := 0
-				pl := state.ProcessLists.Get(state.LLeaderHeight)
-				if pl != nil {
-					pendingEBs = len(pl.NewEBlocks)
-					pendingEntries = len(pl.NewEntries)
-				}
-				status += fmt.Sprintf("Review %d ", len(state.XReview))
-				status += fmt.Sprintf("Holding %d ", len(state.Holding))
-				status += fmt.Sprintf("Commits %d ", state.Commits.Len())
-				status += fmt.Sprintf("Pending EBs %d ", pendingEBs)         // cope with nil
-				status += fmt.Sprintf("Pending Entries %d ", pendingEntries) // cope with nil
-				status += fmt.Sprintf("Acks %d ", len(state.AcksMap))
-				status += fmt.Sprintf("MsgQueue %d ", len(state.msgQueue))
-				status += fmt.Sprintf("InMsgQueue %d ", state.inMsgQueue.Length())
-				status += fmt.Sprintf("InMsgQueue2 %d ", state.inMsgQueue2.Length())
-				status += fmt.Sprintf("APIQueue   %d ", state.apiQueue.Length())
-				status += fmt.Sprintf("AckQueue %d ", len(state.ackQueue))
-				status += fmt.Sprintf("TimerMsgQueue %d ", len(state.timerMsgQueue))
-				status += fmt.Sprintf("NetworkOutMsgQueue %d ", state.networkOutMsgQueue.Length())
-				status += fmt.Sprintf("NetworkInvalidMsgQueue %d ", len(state.networkInvalidMsgQueue))
-				status += fmt.Sprintf("UpdateEntryHash %d ", len(state.UpdateEntryHash))
-				status += fmt.Sprintf("MissingEntries %d ", state.GetMissingEntryCount())
-				status += fmt.Sprintf("WriteEntry %d ", len(state.WriteEntry))
 
-				status += fmt.Sprintf("PL %8d, P %8d, U %8d, S%8d", s.ProcessListProcessCnt, s.StateProcessCnt, s.StateUpdateState, s.ValidatorLoopSleepCnt)
-				state.LogPrintf("executeMsg", "Status %s", status)
-				prev = now
-			}
-		}
+	s := state
+	go s.DoProcessing()
+
+	// this is the message sort
+	for {
 		// Check if we should shut down.
 		select {
 		case <-state.ShutdownChan:
-			fmt.Println("Closing the Database on", state.GetFactomNodeName())
-			state.DB.Close()
-			state.StateSaverStruct.StopSaving()
-			fmt.Println(state.GetFactomNodeName(), "closed")
 			state.IsRunning = false
+			time.Sleep(10 * time.Second) // wait till database close is complete
 			return
 		default:
 		}
 
 		// Look for pending messages, and get one if there is one.
 		var msg interfaces.IMsg
+		select {
+		case min := <-state.tickerQueue:
+			timeStruct.timer(state, min)
+		default:
+		}
 
-		for i := 0; i < 1; i++ {
-			//for state.Process() {}
-			//for state.UpdateState() {}
-			p1 := true
-			p2 := true
-			i1 := 0
-			i2 := 0
-
-			if ValidationDebug {
-				s.LogPrintf("executeMsg", "start validate.process")
-			}
-			for i1 = 0; p1 && i1 < 20; i1++ {
-				p1 = state.Process()
-			}
-			if ValidationDebug {
-				s.LogPrintf("executeMsg", "start validate.updatestate")
-			}
-			for i2 = 0; p2 && i2 < 20; i2++ {
-				p2 = state.UpdateState()
-			}
-			if ValidationDebug {
-				s.LogPrintf("executeMsg", "start validate.messagesort")
-			}
-
-			select {
-			case min := <-state.tickerQueue:
-				timeStruct.timer(state, min)
-			default:
-			}
-
-			msgcnt := 0
-			for i := 0; i < 50; i++ {
-				msg = nil
-				ackRoom := cap(state.ackQueue) - len(state.ackQueue)
-				msgRoom := cap(state.msgQueue) - len(state.msgQueue)
-
-				if ackRoom < 1 || msgRoom < 1 {
-					break // no room
-				}
-				msg = nil // in the i%5==0 we don't want to repeat the prev message
-				if i%5 != 0 {
-					// This doesn't block so it intentionally returns nil, don't log nils
-					msg = state.InMsgQueue().Dequeue()
-					if msg != nil {
-						state.LogMessage("InMsgQueue", "dequeue", msg)
-					}
-				}
-				if msg == nil {
-					// This doesn't block so it intentionally returns nil, don't log nils
-					msg = state.InMsgQueue2().Dequeue()
-					if msg != nil {
-						state.LogMessage("InMsgQueue2", "dequeue", msg)
-					}
-				}
-
+		msgcnt := 0
+		for i := 0; i < 50; i++ {
+			msg = nil
+			if i%5 != 0 {
 				// This doesn't block so it intentionally returns nil, don't log nils
-
+				msg = state.InMsgQueue().Dequeue()
 				if msg != nil {
-					msgcnt++
-					// Sort the messages.
-					if t := msg.Type(); t == constants.ACK_MSG {
-						state.LogMessage("ackQueue", "enqueue", msg)
-						state.ackQueue <- msg //
-					} else {
-						state.LogMessage("msgQueue", fmt.Sprintf("enqueue(%d)", len(state.msgQueue)), msg)
-						state.msgQueue <- msg //
-					}
+					state.LogMessage("InMsgQueue", "dequeue", msg)
 				}
 			}
+			if msg == nil {
+				// This doesn't block so it intentionally returns nil, don't log nils
+				msg = state.InMsgQueue2().Dequeue()
+				if msg != nil {
+					state.LogMessage("InMsgQueue2", "dequeue", msg)
+				}
+			}
+
+			// This doesn't block so it intentionally returns nil, don't log nils
+
+			if msg != nil {
+				msgcnt++
+				// Sort the messages.
+				if t := msg.Type(); t == constants.ACK_MSG {
+					state.LogMessage("ackQueue", "enqueue", msg)
+					state.ackQueue <- msg //
+				} else {
+					state.LogMessage("msgQueue", fmt.Sprintf("enqueue(%d)", len(state.msgQueue)), msg)
+					state.msgQueue <- msg //
+				}
+			}
+		}
+		if ValidationDebug {
+			s.LogPrintf("executeMsg", "stop validate.messagesort sorted %d messages", msgcnt)
+		}
+
+		// if we are not making progress and there are no messages to sort  sleep a bit
+		if state.InMsgQueue().Length() == 0 && state.InMsgQueue2().Length() == 0 {
+			// No messages? Sleep for a bit
+			i := 0
+			for ; i < 10 && state.InMsgQueue().Length() == 0 && state.InMsgQueue2().Length() == 0; i++ {
+				time.Sleep(10 * time.Millisecond)
+				state.ValidatorLoopSleepCnt++
+			}
+
 			if ValidationDebug {
-				s.LogPrintf("executeMsg", "stop validate.messagesort sorted %d messages", msgcnt)
+				s.LogPrintf("executeMsg", "slept %d times", i)
 			}
-
-			// if we are not making progress and there are no messages to sort  sleep a bit
-			if !(p1 || p2) && state.InMsgQueue().Length() == 0 && state.InMsgQueue2().Length() == 0 {
-				// No messages? Sleep for a bit
-				i := 0
-				for ; i < 10 && state.InMsgQueue().Length() == 0 && state.InMsgQueue2().Length() == 0; i++ {
-					time.Sleep(10 * time.Millisecond)
-					state.ValidatorLoopSleepCnt++
-				}
-
-				if ValidationDebug {
-					s.LogPrintf("executeMsg", "slept %d times", i)
-				}
-			}
-
 		}
 
 	}
