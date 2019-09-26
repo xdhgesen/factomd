50f618b46a14d1de5a0b276a609f920bd1982c6e
Veena Gondkar <veena@factom.org>
2019-09-26 14:16:32 -0500

    Added AuditBrainSwap_test simtest

--- simTest/AuditBrainSwap_test.go
+++ simTest/AuditBrainSwap_test.go
@@ -14,21 +14,21 @@ Test brainswapping F <-> L with no auditors
 This test is useful for catching a failure scenario where the timing between
 identity swap is off leading to a stall
 */
-func TestLeaderBrainSwap(t *testing.T) {
+func TestAuditBrainSwap(t *testing.T) {
 	ResetSimHome(t)          // clear out old test home
 	for i := 0; i < 6; i++ { // build config files for the test
 		WriteConfigFile(i, i, "", t) // just write the minimal config
 	}
 
 	params := map[string]string{"--blktime": "10"}
-	state0 := SetupSim("LLLFFF", params, 230, 0, 0, t)
+	state0 := SetupSim("LLLAAFFF", params, 230, 0, 0, t)
 	state3 := engine.GetFnodes()[3].State // Get node 2
 
 	WaitForAllNodes(state0)
 	WaitForBlock(state0, 6)
 
 	// FIXME https://factom.atlassian.net/browse/FD-950 - setting batch > 1 can occasionally cause failure
-	batches := 60 // use odd number to fulfill LFFFLL as end condition
+	batches := 60 // use odd number to fulfill LLLFFAAF as end condition
 
 	for batch := 0; batch < batches; batch++ {
 
@@ -37,26 +37,25 @@ func TestLeaderBrainSwap(t *testing.T) {
 		change := fmt.Sprintf("ChangeAcksHeight = %v\n", target)
 
 		if batch%2 == 0 {
-			WriteConfigFile(1, 5, change, t) // Setup A brain swap between L1 and F5
-			WriteConfigFile(5, 1, change, t)
+			WriteConfigFile(3, 5, change, t) // Setup A brain swap between A3 and F5
+			WriteConfigFile(5, 3, change, t)
 
-			WriteConfigFile(2, 4, change, t) // Setup A brain swap between L2 and F4
-			WriteConfigFile(4, 2, change, t)
+			WriteConfigFile(4, 6, change, t) // Setup A brain swap between A4 and F6
+			WriteConfigFile(6, 4, change, t)
 
 		} else {
 			WriteConfigFile(5, 5, change, t) // Un-Swap
-			WriteConfigFile(1, 1, change, t)
+			WriteConfigFile(3, 3, change, t)
 
 			WriteConfigFile(4, 4, change, t)
-			WriteConfigFile(2, 2, change, t)
+			WriteConfigFile(6, 6, change, t)
 		}
 		WaitForBlock(state3, target)
 		WaitMinutes(state3, 1)
 	}
 
 	WaitBlocks(state0, 1)
-	AssertAuthoritySet(t, "LLLFFF")
-	//AssertAuthoritySet(t, "LFFFLL")
+	AssertAuthoritySet(t, "LLLAAFFF")
 	WaitForAllNodes(state0)
 	ShutDownEverything(t)
 }
