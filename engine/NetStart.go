75dd0ccb1b6e4398f99fb0cbd9b33214370170ee
stackdump <myork@stackdump.com>
2019-10-01 14:21:10 -0500

    configure net in simulation package

--- engine/NetStart.go
+++ engine/NetStart.go
@@ -5,12 +5,10 @@
 package engine
 
 import (
-	"bufio"
 	"bytes"
 	"encoding/binary"
 	"fmt"
 	"github.com/FactomProject/factomd/simulation"
-	"math"
 	"os"
 	"time"
 
@@ -144,6 +142,7 @@ func NetStart(s *fnode.State, p *FactomParams, listenToStdin bool) {
 	fmt.Println(">>>>>>>>>>>>>>>> Listening to Node", p.ListenTo)
 	fmt.Println(">>>>>>>>>>>>>>>>")
 
+	// FIXME: this should be part of engine
 	simulation.AddInterruptHandler(func() {
 		fmt.Print("<Break>\n")
 		fmt.Print("Gracefully shutting down the server...\n")
@@ -363,124 +362,7 @@ func NetStart(s *fnode.State, p *FactomParams, listenToStdin bool) {
 		go networkHousekeeping() // This goroutine executes once a second to keep the proxy apprised of the network status.
 	}
 
-	networkpattern = p.Net
-
-	switch p.Net {
-	case "file":
-		file, err := os.Open(p.Fnet)
-		if err != nil {
-			panic(fmt.Sprintf("File network.txt failed to open: %s", err.Error()))
-		} else if file == nil {
-			panic(fmt.Sprint("File network.txt failed to open, and we got a file of <nil>"))
-		}
-		scanner := bufio.NewScanner(file)
-		for scanner.Scan() {
-			var a, b int
-			var s string
-			fmt.Sscanf(scanner.Text(), "%d %s %d", &a, &s, &b)
-			if s == "--" {
-				simulation.AddSimPeer(fnodes, a, b)
-			}
-		}
-	case "square":
-		side := int(math.Sqrt(float64(p.Cnt)))
-
-		for i := 0; i < side; i++ {
-			simulation.AddSimPeer(fnodes, i*side, (i+1)*side-1)
-			simulation.AddSimPeer(fnodes, i, side*(side-1)+i)
-			for j := 0; j < side; j++ {
-				if j < side-1 {
-					simulation.AddSimPeer(fnodes, i*side+j, i*side+j+1)
-				}
-				simulation.AddSimPeer(fnodes, i*side+j, ((i+1)*side)+j)
-			}
-		}
-	case "long":
-		fmt.Println("Using long Network")
-		for i := 1; i < p.Cnt; i++ {
-			simulation.AddSimPeer(fnodes, i-1, i)
-		}
-		// Make long into a circle
-	case "loops":
-		fmt.Println("Using loops Network")
-		for i := 1; i < p.Cnt; i++ {
-			simulation.AddSimPeer(fnodes, i-1, i)
-		}
-		for i := 0; (i+17)*2 < p.Cnt; i += 17 {
-			simulation.AddSimPeer(fnodes, i%p.Cnt, (i+5)%p.Cnt)
-		}
-		for i := 0; (i+13)*2 < p.Cnt; i += 13 {
-			simulation.AddSimPeer(fnodes, i%p.Cnt, (i+7)%p.Cnt)
-		}
-	case "alot":
-		n := len(fnodes)
-		for i := 0; i < n; i++ {
-			simulation.AddSimPeer(fnodes, i, (i+1)%n)
-			simulation.AddSimPeer(fnodes, i, (i+5)%n)
-			simulation.AddSimPeer(fnodes, i, (i+7)%n)
-		}
-
-	case "alot+":
-		n := len(fnodes)
-		for i := 0; i < n; i++ {
-			simulation.AddSimPeer(fnodes, i, (i+1)%n)
-			simulation.AddSimPeer(fnodes, i, (i+5)%n)
-			simulation.AddSimPeer(fnodes, i, (i+7)%n)
-			simulation.AddSimPeer(fnodes, i, (i+13)%n)
-		}
-
-	case "tree":
-		index := 0
-		row := 1
-	treeloop:
-		for i := 0; true; i++ {
-			for j := 0; j <= i; j++ {
-				simulation.AddSimPeer(fnodes, index, row)
-				simulation.AddSimPeer(fnodes, index, row+1)
-				row++
-				index++
-				if index >= len(fnodes) {
-					break treeloop
-				}
-			}
-			row += 1
-		}
-	case "circles":
-		circleSize := 7
-		index := 0
-		for {
-			simulation.AddSimPeer(fnodes, index, index+circleSize-1)
-			for i := index; i < index+circleSize-1; i++ {
-				simulation.AddSimPeer(fnodes, i, i+1)
-			}
-			index += circleSize
-
-			simulation.AddSimPeer(fnodes, index, index-circleSize/3)
-			simulation.AddSimPeer(fnodes, index+2, index-circleSize-circleSize*2/3-1)
-			simulation.AddSimPeer(fnodes, index+3, index-(2*circleSize)-circleSize*2/3)
-			simulation.AddSimPeer(fnodes, index+5, index-(3*circleSize)-circleSize*2/3+1)
-
-			if index >= len(fnodes) {
-				break
-			}
-		}
-	default:
-		fmt.Println("Didn't understand network type. Known types: mesh, long, circles, tree, loops.  Using a Long Network")
-		for i := 1; i < p.Cnt; i++ {
-			simulation.AddSimPeer(fnodes, i-1, i)
-		}
-
-	}
-
-	var colors []string = []string{"95cde5", "b01700", "db8e3c", "ffe35f"}
-
-	if len(fnodes) > 2 {
-		for i, s := range fnodes {
-			fmt.Printf("%d {color:#%v, shape:dot, label:%v}\n", i, colors[i%len(colors)], s.State.FactomNodeName)
-		}
-		fmt.Printf("Paste the network info above into http://arborjs.org/halfviz to visualize the network\n")
-	}
-
+	simulation.SetupNetwork(p)
 	startServers(true)
 
 	// Anchoring related configurations
@@ -604,8 +486,8 @@ func networkHousekeeping() {
 	}
 }
 
+// add another fnode server during simulation testing
 func AddNode() {
-
 	fnodes := GetFnodes()
 	s := fnodes[0].State
 	i := len(fnodes)
